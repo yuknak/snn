5edf9107c9c7f730c7803a50fa05473432990d90
yuknak <yuknak4@gmail.com>
2020-12-22 12:37:28 +0900

    Fix

--- app/api/v1/thread.rb
+++ app/api/v1/thread.rb
@@ -3,22 +3,18 @@ module V1
 
   ##############################################################################
 
-  class ThreadServerEntity < Grape::Entity
+  class ThreadServerNameEntity < Grape::Entity
     expose :name
   end
-  class ThreadBoardEntity < Grape::Entity
+  class ThreadBoardNameEntity < Grape::Entity
     expose :name
-    expose :server, using: ThreadServerEntity #=>
+    expose :server, using: ThreadServerNameEntity #=>
   end
-  class ThreadEntity < Grape::Entity
+  class ThreadWithBoardNameEntity < Grape::Entity
     format_with(:iso_timestamp) { |dt| dt.nil? ? nil : dt.iso8601 }
-    expose :id
-    expose :board_id
-    expose :board, using: ThreadBoardEntity # =>
+    expose :board, using: ThreadBoardNameEntity # =>
     expose :tid
     expose :title
-    expose :mirror_ver
-    expose :mirror_order
     with_options(format_with: :iso_timestamp) do
       expose :mirrored_at
     end
@@ -26,15 +22,11 @@ module V1
     expose :res_added
     expose :res_speed
     expose :res_percent
-    with_options(format_with: :iso_timestamp) do
-      expose :created_at
-      expose :updated_at
-    end
   end
   class ThreadBoardEntity < Grape::Entity
     format_with(:iso_timestamp) { |dt| dt.nil? ? nil : dt.iso8601 }
-    expose :id
     expose :name
+    expose :server, using: ThreadServerNameEntity #=>
     expose :title
     with_options(format_with: :iso_timestamp) do
       expose :mirrored_at
@@ -42,17 +34,32 @@ module V1
     expose :res_added
     expose :res_speed
   end
-  class ThreadsEntity < Grape::Entity
+  class ThreadsWithPagingEntity < Grape::Entity
     expose :page
     expose :per_page
     expose :total_pages
     expose :total  
     expose :board, using: ThreadBoardEntity # =>
-    expose :data, using: ThreadEntity # data[] array =>
+    expose :data, using: ThreadWithBoardNameEntity # data[] array =>
   end
 
   ##############################################################################
-
+  class ThreadEntity < Grape::Entity
+    format_with(:iso_timestamp) { |dt| dt.nil? ? nil : dt.iso8601 }
+    expose :tid
+    expose :title
+    with_options(format_with: :iso_timestamp) do
+      expose :mirrored_at
+    end
+    expose :res_cnt
+    expose :res_added
+    expose :res_speed
+    expose :res_percent
+  end
+  class ThreadsEntity < Grape::Entity
+    expose :board, using: ThreadBoardEntity # =>
+    expose :data, using: ThreadEntity # data[] array =>
+  end
   class ThreadTopEntity < Grape::Entity
     expose :data, using: ThreadsEntity # data[] array =>
   end
@@ -83,42 +90,56 @@ module V1
       ##########################################################################
 
       def get_latest
+        params[:per_page] = 50
         threads = FiveCh::Thread.all.includes([board: :server])
-          .order(tid: 'DESC').limit(50)
+          .order(tid: 'DESC')
         threads = ransack_index(threads)
-        present threads, with: ThreadsEntity  
+        present threads, with: ThreadsWithPagingEntity  
       end
 
       def get_today
+        params[:per_page] = 50
         threads = FiveCh::Thread.where(
           'tid >= ?', epoch_today).includes([board: :server])
-            .order(tid: 'DESC').limit(50)
+            .order(tid: 'DESC')
         threads = ransack_index(threads)
-        present threads, with: ThreadsEntity  
+        present threads, with: ThreadsWithPagingEntity  
       end
 
       def get_yesterday
+        params[:per_page] = 50
         threads = FiveCh::Thread.where(
           'tid >= ? AND tid < ?', epoch_yesterday, epoch_today).includes([board: :server])
-            .order(tid: 'DESC').limit(50)
+            .order(tid: 'DESC')
         threads = ransack_index(threads)
-        present threads, with: ThreadsEntity  
+        present threads, with: ThreadsWithPagingEntity  
       end
 
       def get_festival
+        params[:per_page] = 50
         threads = FiveCh::Thread.all.includes([board: :server])
-          .order(res_speed: 'DESC', mirror_order: 'ASC').limit(50)
+          .order(res_speed: 'DESC', mirror_order: 'ASC')
         threads = ransack_index(threads)
-        present threads, with: ThreadsEntity  
+        present threads, with: ThreadsWithPagingEntity  
       end
 
       ##########################################################################
 
       def get_top
-        threads = FiveCh::Thread.all.includes([board: :server])
-        .order(tid: 'DESC').limit(50)
-        threads = ransack_index(threads)
-        present threads, with: ThreadsEntity  
+        data = []
+        params[:per_page] = 3
+        FiveCh::Board.all.each do |board|
+          threads = FiveCh::Thread.where(
+            board_id: board.id, mirror_ver: board.mirror_ver)
+              .includes([board: :server])
+              .order(res_speed: 'DESC', mirror_order: 'ASC')
+          threads = ransack_index(threads)
+          threads[:board] = board
+          data.push(threads)
+        end
+        top_data = {}
+        top_data[:data] = data
+        present top_data, with: ThreadTopEntity
       end
 
     end
@@ -146,17 +167,21 @@ module V1
         # a special data structure
         elsif board_name == 'top' then
           get_top
+          #get_latest
 
         # process each board
         else
+          params[:per_page] = 50
+
           board = get_board(board_name)
           threads = FiveCh::Thread.where(
             board_id: board.id, mirror_ver: board.mirror_ver)
-              .order(res_speed: 'DESC', mirror_order: 'ASC').limit(50)
+              .order(res_speed: 'DESC', mirror_order: 'ASC')
+            .includes([board: :server])
           threads = ransack_index(threads)
           threads[:board] = board
           #puts threads.to_json
-          present threads, with: ThreadsEntity
+          present threads, with: ThreadsWithPagingEntity
         end
       end
 
