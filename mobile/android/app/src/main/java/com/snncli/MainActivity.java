b4cd40dde65b7b2fb73ef908864a0f8c664d0f53
yuknak <yuknak4@gmail.com>
2021-01-04 16:36:46 +0900

    Change release info

--- mobile/android/app/src/main/java/com/snncli/MainActivity.java
+++ mobile/android/app/src/main/java/com/snncli/MainActivity.java
@@ -1,4 +1,4 @@
-package com.snncli;
+package biz.tetraserve.snncli;
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import org.devio.rn.splashscreen.SplashScreen;
--- mobile/android/app/src/main/java/com/snncli/MainApplication.java
+++ mobile/android/app/src/main/java/com/snncli/MainApplication.java
@@ -1,4 +1,4 @@
-package com.snncli;
+package biz.tetraserve.snncli;
 
 import android.app.Application;
 import android.content.Context;
