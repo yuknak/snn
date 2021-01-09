5b34ab55335d675cabf7af31eb0f5aa62de07130
yuknak <yuknak4@gmail.com>
2021-01-09 14:50:02 +0900

    Set public_file_server true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,7 @@ Rails.application.configure do
   config.hosts << ".supernn.net"
   config.hosts << ".tetraserve.biz"
   config.hosts << ".tetraserve.local"
+  config.hosts.clear
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -24,7 +25,8 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  #config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = true
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
