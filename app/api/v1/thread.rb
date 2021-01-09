a8d1327132c3e8879b7fa40f2f2051c85a17247d
yuknak <yuknak4@gmail.com>
2021-01-09 10:28:55 +0900

    Add cache to API

--- app/api/v1/thread.rb
+++ app/api/v1/thread.rb
@@ -5,7 +5,7 @@ module V1
   # IMPORTANT NOTICE)
   # 1)
   # About cache(Rails.cache.fetch)
-  # DB functions behaviors are defenitely
+  # DB function behaviors are defenitely
   # DIFFERENT if they are in Rails.cache.fetch block!!!
   # because of Rails optimization.
   # This is a known problem. Generally, we avoid this by using
@@ -93,10 +93,6 @@ module V1
 
     helpers do
 
-      def get_board(name)
-        FiveCh::Board.find_by(name: name)
-      end
-
       def epoch_today
         t = Time.now
         Time.local(0,0,0,t.day,t.mon,t.year,nil,nil,false,nil).to_i
@@ -113,45 +109,110 @@ module V1
       end
 
       ##########################################################################
+      # Process thread records in order to be placed in
+      # Rails.cache.fetch block
+      def proc_threads(threads)
+        threads = ransack_index(threads)
+        threads = JSON.parse(threads.to_json)
+        data = []
+        threads['data'].each do |thread_data|
+          board = FiveCh::Board.find(thread_data['board_id'])
+          server = FiveCh::Server.find(board['server_id'])
+          thread = JSON.parse(thread_data.to_json)
+          board = JSON.parse(board.to_json)
+          board['server'] = { name: server['name'] }
+          thread['board'] = board
+          data.push(thread)
+        end
+        threads['data'] = data
+        threads        
+      end
+
+      ##########################################################################
 
       def get_latest
         # get from a week ago
         threads = FiveCh::Thread.where(
           'tid >= ?', epoch_aweekago).includes([board: :server])
             .order(tid: 'DESC')
-        threads = ransack_index(threads)
-        present threads, with: ThreadsWithPagingEntity  
+        proc_threads(threads)
+        #present threads, with: ThreadsWithPagingEntity
       end
+
+      def cache_latest
+        Rails.cache.fetch("/latest", expires_in: 20.seconds) do
+          get_latest
+        end
+      end
+
+      ##########################################################################
 
       def get_today
         threads = FiveCh::Thread.where(
           'tid >= ?', epoch_today).includes([board: :server])
             .order(res_speed_max: 'DESC', tid: 'ASC')
-        threads = ransack_index(threads)
-        present threads, with: ThreadsWithPagingEntity  
+        proc_threads(threads)
+        #present threads, with: ThreadsWithPagingEntity  
       end
+
+      def cache_today
+        Rails.cache.fetch("/today", expires_in: 20.seconds) do
+          get_today
+        end
+      end
+
+      ##########################################################################
 
       def get_yesterday
         threads = FiveCh::Thread.where(
           'tid >= ? AND tid < ?', epoch_yesterday, epoch_today).includes([board: :server])
             .order(res_speed_max: 'DESC', tid: 'ASC')
-        threads = ransack_index(threads)
-        present threads, with: ThreadsWithPagingEntity  
+        proc_threads(threads)
+        #present threads, with: ThreadsWithPagingEntity  
       end
+
+      def cache_yesterday
+        Rails.cache.fetch("/yesterday", expires_in: 20.seconds) do
+          get_yesterday
+        end
+      end
+
+      ##########################################################################
 
       def get_week
         threads = FiveCh::Thread.where(
           'tid >= ?', epoch_aweekago).includes([board: :server])
             .order(res_speed_max: 'DESC', tid: 'ASC')
-        threads = ransack_index(threads)
-        present threads, with: ThreadsWithPagingEntity  
+        proc_threads(threads)
+        #present threads, with: ThreadsWithPagingEntity  
       end
 
-      # TODO: deprecated
-      def get_festival
-        get_week
+      def cache_week
+        Rails.cache.fetch("/week", expires_in: 20.seconds) do
+          get_week
+        end
       end
 
+      ##########################################################################
+
+      def get_category(board_name)
+        board = FiveCh::Board.find_by(name: board_name)
+        threads = FiveCh::Thread.where(
+          board_id: board.id, mirror_ver: board.mirror_ver)
+            .order(res_percent: 'DESC', mirror_order: 'ASC')
+          .includes([board: :server])
+        threads = proc_threads(threads)
+        threads['board'] = JSON.parse(board.to_json)
+        threads
+        #present threads, with: ThreadsWithPagingEntity
+      end
+
+      def cache_category(board_name)
+        Rails.cache.fetch("/category/#{board_name}", expires_in: 20.seconds) do
+          get_category(board_name)
+        end
+      end
+      
       ##########################################################################
         
       @@top_boards = [
@@ -169,7 +230,7 @@ module V1
       def get_top
         data = []
         @@top_boards.each do |top_board|
-          board = get_board(top_board[:name])
+          board = FiveCh::Board.find_by(name: top_board[:name])
           server = FiveCh::Server.find(board.server_id)
           params[:per_page] = top_board[:count]
           threads = FiveCh::Thread.where(
@@ -177,24 +238,24 @@ module V1
               .includes([board: :server])
               .order(res_percent: 'DESC', mirror_order: 'ASC')
           threads = ransack_index(threads)
-          # DB object is DIFFERENT and also, we cannot add fields!
-          threads = JSON.parse(threads.to_json) # hack!
-          board = JSON.parse(board.to_json) # hack!
-          board[:server] = { name: server['name'] } # can add
-          threads[:board] = board # can add
+          threads = JSON.parse(threads.to_json)
+          board = JSON.parse(board.to_json)
+          board[:server] = { name: server['name'] }
+          threads[:board] = board
           data.push(threads)
         end
         top_data = {}
         top_data[:data] = data
-        top_data.to_json
+        top_data
+        #present top_data, with: ThreadTopEntity
       end
 
       def cache_top
-        Rails.cache.fetch("/top", expires_in: 15.seconds) do
+        Rails.cache.fetch("/top", expires_in: 20.seconds) do
           get_top
         end
       end
-
+      
       ##########################################################################
 
       def get_search
@@ -231,36 +292,36 @@ module V1
       get ':id' do
 
         board_name = params[:id]
+        page = params.dig(:page)
 
         # process special screens
-        if (board_name == 'latest') then
-          get_latest
+        if (board_name == 'latest' && page.present?) then
+          present get_latest
+        elsif (board_name == 'latest') then
+          present cache_latest
+        elsif (board_name == 'today' && page.present?) then
+          present get_today
         elsif (board_name == 'today') then
-          get_today
+          present cache_today
+        elsif (board_name == 'yesterday' && page.present?) then
+          present get_yesterday
         elsif (board_name == 'yesterday') then
-          get_yesterday
+          present cache_yesterday
+        elsif (board_name == 'week' && page.present?) then
+          present get_week
         elsif (board_name == 'week') then
-          get_week
-        elsif (board_name == 'festival') then # TODO: deprecated
-          get_festival
+          present cache_week
 
         # for top page, having special data strucure
         elsif board_name == 'top' then
           # always no param(no page info)
-          data = JSON.parse(cache_top)
-          present data
+          present cache_top
 
         # process each board, normal pattern
+        elsif (page.present?) then
+          present get_category(board_name)
         else
-          board = get_board(board_name)
-          threads = FiveCh::Thread.where(
-            board_id: board.id, mirror_ver: board.mirror_ver)
-              .order(res_percent: 'DESC', mirror_order: 'ASC')
-            .includes([board: :server])
-          threads = ransack_index(threads)
-          threads[:board] = board
-          #puts threads.to_json
-          present threads, with: ThreadsWithPagingEntity
+          present cache_category(board_name)
         end
       end
 
